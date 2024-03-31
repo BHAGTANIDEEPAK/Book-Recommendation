9a82939f4e16255e97b118125df9c801f934d041
BHAGTANIDEEPAK <bhagatnideepak77@gmail.com>
2024-03-31 17:34:06 +0530

    intial commit

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -9,10 +9,17 @@ const SearchBar = ({ onSearch }) => {
   const handleTitleSearch = () => {
     onSearch({ query: title });
   };
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      handleTitleSearch();
+    }
+  };
   return (
     <div className='search-bar'>
-      <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Search by title" />
-      <button onClick={handleTitleSearch}>Search Title</button>
+      <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} 
+      onKeyPress={handleKeyPress}
+      placeholder="Search by title" />
+      <button onClick={handleTitleSearch} >Search Title</button>
     </div>
   );
 };
